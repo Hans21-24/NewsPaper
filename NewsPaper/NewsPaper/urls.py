9dee40631c096c5d61d7fa79474314866d337bbe
Hans <vjxbkjdjdj@mail.ru>
2023-10-31 13:48:28 +0700

    После чего подключим urls приложения accounts в главном приложении.

--- NewsPaper/NewsPaper/urls.py
+++ NewsPaper/NewsPaper/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/", include("accounts.urls")),  # Добавили эту строчку
     # Делаем так, чтобы все адреса из нашего приложения (simpleapp/urls.py)
     # подключались к главному приложению с префиксом products/.
     path('news/', include('news.urls')),
