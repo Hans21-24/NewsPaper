bb0c248b42c20a486aa8321ec665774d8601d0b0
Hans <vjxbkjdjdj@mail.ru>
2023-11-01 14:09:09 +0700

    Лишняя строка в url

--- NewsPaper/NewsPaper/urls.py
+++ NewsPaper/NewsPaper/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
     path("accounts/", include("allauth.urls")),  # Оставили только allauth
     # Делаем так, чтобы все адреса из нашего приложения (simpleapp/urls.py)
     # подключались к главному приложению с префиксом products/.
